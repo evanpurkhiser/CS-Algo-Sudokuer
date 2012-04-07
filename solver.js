3ddebc00576dc1423c8fdb66d92afbe43807c104
Evan Purkhiser <evanpurkhiser@gmail.com>
2012-04-06 22:12:11 -0400

    Setup the basic singleton solver object, it doesn't do much just yet but the basic structure is there

--- solver.js
+++ solver.js
@@ -1,12 +1,27 @@
-// Well start with a unsolved puzzle
-var puzzle = [];
+/**
+ * Singleton puzzle solver object
+ *
+ * This object is used to solve sudoku puzzles.
+ * The only method exposed will is the solve method
+ * which accepts a two dimensional array of sudoku
+ * cell values, where empty cells are defined as any
+ * cell that contains the value zero.
+ */
+var solver = function()
+{
+	var solve = function(unsolvedPuzzle)
+	{
+		return unsolvedPuzzle;
+	};
 
-// Puzzle input format documentation:
-// All puzzles weather solved or unsolved should be contained in a two
-// dimensional array. If the puzzle is unsolved, then cells that do not
-// contain a solved sudoku will container a `0`. Since zero is a invalid
-// number in the game of sudoku, we will be using this value as our null
-// character to determine what an empty cell is.
+	return {
+		'solve' : solve
+	};
+}();
+
+
+// Setup a unsolved puzzle
+var puzzle = [];
 
 // puzzle    1  2  3  4  5  6  7  8  9       1  2  3  4  5  6  7  8  9
 puzzle[0] = [9, 0, 0, 1, 0, 0, 0, 7, 5]; // [9, 2, 4, 1, 8, 6, 3, 7, 5]
@@ -18,3 +33,6 @@ puzzle[5] = [0, 3, 7, 4, 1, 0, 9, 0, 0]; // [5, 3, 7, 4, 1, 2, 9, 6, 8]
 puzzle[6] = [0, 0, 2, 0, 0, 0, 6, 8, 4]; // [1, 7, 2, 5, 3, 9, 6, 8, 4]
 puzzle[7] = [0, 4, 0, 0, 7, 1, 0, 0, 0]; // [8, 4, 9, 6, 7, 1, 5, 3, 2]
 puzzle[8] = [6, 5, 0, 0, 0, 8, 0, 0, 9]; // [6, 5, 3, 2, 4, 8, 7, 1, 9]
+
+// Solve the puzzle
+console.log(solver.solve(puzzle));
