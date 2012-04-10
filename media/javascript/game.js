c94591d56f8e2acad6561e90887e52bafaf5b776
Evan Purkhiser <evanpurkhiser@gmail.com>
2012-04-09 22:52:34 -0400

    Fixed a bug where cell values were being improperly set in the puzzle as strings

--- media/javascript/game.js
+++ media/javascript/game.js
@@ -21,7 +21,7 @@ $(function()
 			var index = cell.name.match(/value\[([0-9])\]\[([0-9])\]/);
 
 			// Set the value of the cell in the puzzle array
-			puzzle[index[1]][index[2]] = cell.value || 0;
+			puzzle[index[1]][index[2]] = parseInt(cell.value) || 0;
 		});
 
 		return puzzle;
