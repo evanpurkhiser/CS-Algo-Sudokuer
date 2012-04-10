4794a00b0add575dcedab360bac6512b9c2718ab
Evan Purkhiser <evanpurkhiser@gmail.com>
2012-04-10 19:32:06 -0400

    Add board clear and value checking to the gamejs

--- media/javascript/game.js
+++ media/javascript/game.js
@@ -65,6 +65,14 @@ $(function()
 		board.setPuzzle(sudokuPuzzles.getRandom());
 	});
 
+	/**
+	 * When clicking on the "Solve the puzzle" button
+	 * grab the current puzzle on the board and convert
+	 * it to a puzzle arraay, then solve the puzzle.
+	 * After solving set teh puzzle to the board
+	 *
+	 * Also report running times in an alert
+	 */
 	$('#solve').bind('click', function()
 	{
 		// Solve the puzzle and get the resultant object
@@ -72,6 +80,32 @@ $(function()
 
 		// Set the board to the solved puzzle
 		board.setPuzzle(solved.puzzle);
+
+		// Alert how long the solution took
+		alert('Puzzle solved in ' + solved.runningTime + 'ms');
+	});
+
+	/**
+	 * When clicking on the "Clear the board" button
+	 * reset all of the input values on the board
+	 */
+	$('#clear').bind('click', function()
+	{
+		board.find('input').val('');
+	});
+
+	/**
+	 * When entering text into any of the boards input
+	 * fields, don't allow any lettrs or numbers greater
+	 * than 9.
+	 */
+	board.find('input').bind('keyup', function()
+	{
+		// Make sure the number is valid
+		if ( ! /^[1-9]$/.test(this.value))
+		{
+			this.value = '';
+		}
 	});
 
 });
